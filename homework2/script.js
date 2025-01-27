79e65335ec2591c7b66ff3b1de6b94c688d2c96c
Stella Smith <112897475+Ste11ajames@users.noreply.github.com>
2025-01-27 11:13:53 -0700

    commit to main

---
+++ homework2/script.js
@@ -0,0 +1,22 @@
+
+console.log("My Favorite Websites:");
+console.log("1. https://www.vogue.com");
+console.log("2.https://www.girlsgogames.com ");
+console.log("3.https://www.youtube.com");
+// My favorite Websites
+
+
+console.log("My Favorite Games:");
+console.log("1. Hide and Seek Tag");
+console.log("2. Connections on the NYT mini games ");
+console.log("3. Super Mario Bros on the Wii");
+// My favorite Games 
+
+console.log("My Favorite Artists and Their Importance:");
+console.log("1. Cindy Sherman - Her photographs and art are considered to be a major influence on socially critical photography");
+console.log("2. The Beatles - Influenced thousands of artists to come in music, fashion and social issues. ");
+console.log("3. Hayao Miyazaki - Introduced the world to Japanese animation and created the cutest most badass women characters");
+// My favorite Artists and Why they are important
+
+
+var name = prompt('what is your favorite sweet treat?')
