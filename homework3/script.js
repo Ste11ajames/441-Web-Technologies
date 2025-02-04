0acc598e46353e79dc636d3286407db5cda4a531
Stella Smith <112897475+Ste11ajames@users.noreply.github.com>
2025-02-03 18:33:21 -0700

    push

--- homework3/script.js
+++ homework3/script.js
@@ -2,7 +2,7 @@ function choosePath(choice) {
     let storyText = document.getElementById("story-text");
 
     if (choice === "eat") {
-        storyText.innerHTML = "The berry tastes amazing! But suddenly, your stomach churns. Do you drink water or wait it out?";
+        storyText.innerHTML = "The Fruit tastes amazing! But suddenly, your stomach churns. Do you drink water or wait it out?";
         updateButtons("Drink water", "Wait it out", "water", "wait");
     } 
     else if (choice === "ignore") {
@@ -10,15 +10,15 @@ function choosePath(choice) {
         updateButtons("Drink the soup", "Politely decline", "soup", "decline");
     } 
     else if (choice === "water") {
-        storyText.innerHTML = "You drink water... but the poison reacts violently. You collapse. Game over!";
+        storyText.innerHTML = "You drink water... but the poison reacts violently. You vommit and fall to the ground. DEATH!";
         updateButtons("Restart", "", "restart", "");
     } 
     else if (choice === "wait") {
-        storyText.innerHTML = "You endure the pain... and somehow, you survive! The berry gave you immunity to all poisons. What a smart guy you are! Winner winner chicken dinner!";
+        storyText.innerHTML = "You endure the pain... and somehow, you survive! The fruit gave you immunity to all poisons. What a smart guy you are! Winner winner chicken dinner!";
         updateButtons("Restart", "", "restart", "");
     } 
     else if (choice === "soup") {
-        storyText.innerHTML = "The soup is warm and delicious. Unfortunately, it was also poisoned. You feel dizzy... Game over!";
+        storyText.innerHTML = "The soup is warm and delicious. Unfortunately, it was also poisoned. You feel dizzy... DEATH!";
         updateButtons("Restart", "", "restart", "");
     } 
     else if (choice === "decline") {
@@ -26,8 +26,8 @@ function choosePath(choice) {
         updateButtons("Restart", "", "restart", "");
     } 
     else if (choice === "restart") {
-        storyText.innerHTML = "You find a strange-looking berry on the ground. It smells sweet. What do you do?";
-        updateButtons("Eat the berry", "Ignore it", "eat", "ignore");
+        storyText.innerHTML = "You find a strange smelling fruit-like thing on the ground. It smells of fresh linen. What do you do?";
+        updateButtons("Eat the strange fruit", "Ignore it", "eat", "ignore");
     }
 }
 
