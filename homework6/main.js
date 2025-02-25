841b26d75c8846ed819439f5404f5a46d6c36703
Stella Smith <112897475+Ste11ajames@users.noreply.github.com>
2025-02-24 18:45:31 -0700

    push

---
+++ homework6/main.js
@@ -0,0 +1,34 @@
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8"];
+var blankImagePath = "images/star.jpg";
+var actualImages = [];
+
+function printBlanks() {
+    createRandomImageArray();
+    for (var i = 0; i < imageTags.length; i++) {
+        document.getElementById(imageTags[i]).src = blankImagePath;
+    }
+}
+
+function createRandomImageArray() {
+    var actualImagePath = ["images/tortoise01.jpg", "images/tortoise02.jpg", "images/tortoise03.jpg", "images/tortoise04.jpg"];
+    var count = [0, 0, 0, 0]; //count matches the number of images
+
+    actualImages = []; 
+
+    while (actualImages.length < 8) {
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length);
+        
+        if (count[randomNumber] < 2) { 
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber]++;
+        }
+    }
+}
+
+function flipImage(number) {
+    if (actualImages.length === 8) { 
+        document.getElementById(imageTags[number]).src = actualImages[number];
+    } else {
+        console.error("actualImages not initialized correctly.");
+    }
+}
